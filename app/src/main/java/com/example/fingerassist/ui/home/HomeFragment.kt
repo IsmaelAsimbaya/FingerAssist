850bde2e41b784d57c27e5678e6997b22b43c1c0
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2022-08-18 08:56:14 -0500

    autenticacion con huella

--- app/src/main/java/com/example/fingerassist/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fingerassist/ui/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.fingerassist.databinding.FragmentHomeBinding
@@ -16,6 +18,8 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var canAuthenticate = false
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +32,32 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        setupAuth()
+
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+
+
+
+
         return root
+    }
+
+    private fun setupAuth() {
+        if (BiometricManager.from(requireContext()).canAuthenticate(
+                BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        or BiometricManager.Authenticators.DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS){
+            canAuthenticate = true
+            promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Esperando confirmacion...")
+                .setSubtitle("Confirma utilizando su huella")
+                .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                .build()
+        }
     }
 
     override fun onDestroyView() {
