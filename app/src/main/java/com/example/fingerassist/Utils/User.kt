4010af1ac250fdbe02c1be9e5c6255b2caabbc58
Roberth Troya <39345977+CoilBetrox@users.noreply.github.com>
2022-08-29 00:25:35 -0500

    updateUpdate

--- app/src/main/java/com/example/fingerassist/Utils/User.kt
+++ app/src/main/java/com/example/fingerassist/Utils/User.kt
@@ -4,4 +4,5 @@ data class User (var dia: String ?= "",
                  var horario: String ?= "",
                  var entrada: String ?= "",
                  var salida: String ?= "",
-                 var atraso: String ?= "")
+                 var atraso: String ?= "",
+                 var name: String ?= "")
